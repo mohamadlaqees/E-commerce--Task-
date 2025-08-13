2d0a4497a322bbae3887dab47eb2597a560c8cee
mohamadlaqees <mohamadlk24@gmail.com>
2025-08-14 01:23:36 +0300

    fix UI clasess

--- components/CardComponent.tsx
+++ components/CardComponent.tsx
@@ -5,7 +5,7 @@ import classes from './CardComponent.module.css'
 import { HeartFilled, HeartOutlined, ShoppingCartOutlined, ShoppingFilled } from '@ant-design/icons';
 import { CardComponentProps } from '@/lib/interfaces';
 
-const CardComponent: React.FC<CardComponentProps> = ({ productInfo, onAddToCart, onToggleFavorite, isFavorited, isCartItem }) => {
+const CardComponent: React.FC<CardComponentProps> = ({ productInfo, onAddToCart, onToggleFavorite, isFavorited, isCartItem, dictionary }) => {
 
     const handleIconclick = (e: React.MouseEvent, actions: 'cart' | 'favorite') => {
         e.stopPropagation();
@@ -21,19 +21,21 @@ const CardComponent: React.FC<CardComponentProps> = ({ productInfo, onAddToCart,
     return (
         <Card className={classes.card} title={productInfo.title} variant="borderless" hoverable
             actions={[
-                <Tooltip title={`${isFavorited ? `${productInfo.removeFromFavoriteButton}` : `${productInfo.addToFavoriteButton}`}`}>
+                <Tooltip title={`${isFavorited ? `${dictionary['Remove from favorite']}` : `${dictionary['Add to favorite']}`}`}>
                     <Button type='text' shape='circle' icon={isFavorited ? <HeartFilled className={classes.favoritedIcon} /> : <HeartOutlined />} onClick={(e) => handleIconclick(e, 'favorite')} />
                 </Tooltip>,
-                <Tooltip title={`${isCartItem ? `${productInfo.removeFromCartButton}` : `${productInfo.addToCartButton}`} `}>
+                <Tooltip title={`${isCartItem ? `${dictionary['Remove from cart']}` : `${dictionary['Add to cart']}`} `}>
                     <Button type='text' shape='circle' icon={isCartItem ? <ShoppingFilled className={classes.shoppingIcon} /> : <ShoppingCartOutlined />} onClick={(e) => handleIconclick(e, 'cart')} />
                 </Tooltip>
             ]}  >
             <img className={classes.cardImage} src={`/${productInfo.image}`} alt={productInfo.title} />
             <div className={classes.content}>
-                {productInfo.description}
-            </div>
-            <div className={classes.price}>
-                {productInfo.price} $
+                <div className={classes.description}>
+                    {productInfo.description}
+                </div>
+                <div className={classes.price}>
+                    {productInfo.price} $
+                </div>
             </div>
         </Card>)
 }
